e068853bd522058dfc6a6f57e8628848326d67c9
Maximillian Schulte <maximilliangschulte@gmail.com>
2019-07-29 09:33:34 -0500

    computing offsets of offsets

--- memconf.py
+++ memconf.py
@@ -1,6 +1,5 @@
 import toml
 
-
 """
 The responsibilities of this python script are to take the ssbm memory
 configuration and parse it.  Then hard memory address are calculated from the
@@ -15,6 +14,66 @@ def _offset_addr(addr: str, offset: str):
     return "%x" % (int(addr, base=16) + int(offset, base=16))
 
 
+import time
+
+
+def __get_offsets(blocks: dict, addr: str, offsets: dict, cat: str):
+    result = {}
+    cat_trace = cat.split(".")
+    if type(cat_trace) == list:
+        cat = cat_trace[-1]
+
+    new_offsets = {}
+    for offset in offsets[cat]:
+        # pointers are only stored in the config as strings
+        if type(offsets[cat][offset]) == str:
+            sub = _get_offsets(
+                offsets.copy(),
+                offset,
+                offsets.copy(),
+                ".".join(cat_trace) + "." + offsets[cat][offset],
+            )
+            new_offsets.update(sub)
+            continue
+        new_offsets[offset] = offsets[cat][offset]
+
+    for offset in new_offsets:
+        if type(new_offsets[offset]) == str:
+            continue
+        new_addr = _offset_addr(addr, offset)
+        result[new_addr] = new_offsets[offset].copy()
+        print(list(map(lambda x: offsets[x], cat_trace)))
+        print(offsets)
+        print(cat_trace)
+        result[new_addr]["label"] = (
+            ".".join(list(map(lambda x: offsets[x]["label"], cat_trace)))
+            + "."
+            + new_offsets[offset]["label"]
+        )
+    return result
+
+
+def _get_offsets(block_sect: dict, addr: str, offsets: dict, common_cat: str):
+    result = {}
+    addr_label = block_sect[addr]
+    # get all pointers from dictionary and get its offsets
+    new_offsets = {}
+    for offset in offsets[common_cat]:
+        # if offsets is of type pointer get its offsets
+        if type(offsets[common_cat][offset]) == str:
+            offset_offsets = _get_offsets(offsets[common_cat], offset, offsets, offsets[common_cat][offset])
+            new_offsets.update(offset_offsets)
+            continue
+        new_offsets[offset] = offsets[common_cat][offset].copy()
+    for offset in new_offsets:
+        if type(new_offsets[offset]) == str:
+            continue
+        new_addr = _offset_addr(addr, offset)
+        result[new_addr] = new_offsets[offset].copy()
+        result[new_addr]["label"] = addr_label + "." + new_offsets[offset]["label"]
+    return result
+
+
 # load_config loads the toml configuation file and calculates all offset
 # addresses.
 def load_config(path: str):
@@ -27,16 +86,12 @@ def load_config(path: str):
     addresses = config["addresses"]
     blocks = config["blocks"]
     offsets = config["offsets"]
+
     # compute offset data for each block data value
     for cat in blocks:
         for addr in blocks[cat]:
-            for offset in offsets[cat]:
-                new_addr = _offset_addr(addr, offset)
-                addresses[new_addr] = offsets[cat][offset].copy()
-                addresses[new_addr]["label"] = (
-                    blocks[cat][addr] + "." + offsets[cat][offset]["label"]
-                )
-
+            offset_addresses = _get_offsets(blocks[cat], addr, offsets, cat)
+            addresses.update(offset_addresses)
     # swap memory addresses and labels to allow data indexing both ways
     labels = {}
     for addr in addresses:
--- memwatcher.py
+++ memwatcher.py
@@ -18,24 +18,23 @@ MEMWATCH_DIR = "/home/maximillian/.local/share/dolphin-emu/MemoryWatcher/"
 
 # TODO: debug remove in production
 DEBUG_LABELS = {
-    # "global_frame_counter",
-    # "p1.x",
-    "p1.y",
-    # "p1.direction",
-    # "p1.percentage",
-    # "p2.x",
-    # "p2.y",
-    # "p2.direction",
-    # "p2.percentage",
-    # "match.",
-    # "match.player_last_died",
-    # "match.finished",
-    # "match.frame_count",
-    # "match.seconds_left",
-    # "stage.blastzone_left",
-    # "stage.blastzone_right",
-    # "stage.blastzone_top",
-    # "stage.blastzone_bottom",
+    # "stage.blastzone_bottom", # check
+    # "stage.blastzone_top", # check
+    # "stage.blastzone_right", # check
+    # "stage.blastzone_left", # check
+    # "match.frame_count",  # check
+    # "match.finished", # check
+    # "p2.body_state", # bust
+    # "p1.body_state", # bust
+    # "p2.percentage", # check
+    # "p2.facing_direction", # bust
+    # "p2.y", # check
+    # "p2.x", # check
+    # "p1.percentage", # check
+    # "p1.facing_direction", # bust
+    # "p1.y", # check
+    # "p1.x", # check
+    # "p1.entity.body_state",
 }
 
 
@@ -53,6 +52,7 @@ def main():
     )
     os.lseek(memory_locations_fd, 0, 0)
     for addr in sorted(list(address_index.keys()), reverse=True):
+        print(addr, address_index[addr]["label"])
         os.write(memory_locations_fd, bytes(addr + "\n", encoding="utf-8"))
     os.close(memory_locations_fd)
 
@@ -75,7 +75,8 @@ def main():
     print(df.T)
 
     print("listening at %s..." % socket_path)
-    while True:
+    done = False
+    while not done:
         try:
             # data = sock_fd.recv(1024)
             message = sock_fd.recv(1024 * 8)
@@ -84,13 +85,24 @@ def main():
             data = address_data_pairs(message)
             # print(data.shape)
             for addr, hex_value in data:
-                dtype = type(address_index[addr]["value"])
-                if dtype == float:
-                    hex_value = unpack(">f", bytes.fromhex(hex_value))[0]
-                elif dtype == int:
-                    hex_value = unpack(">i", bytes.fromhex(hex_value))[0]
+                dtype = None
+                try:
+                    dtype = address_index[addr]["type"]
+                except:
+                    print(addr)
+                    continue
+                if dtype == "float":
+                    hex_value = unpack(">f", bytes.fromhex(hex_value.zfill(8)))[0]
+                elif dtype == "int":
+                    hex_value = int(hex_value, base=16)
+                elif dtype == "short":
+                    # hex_value = unpack(">i", bytes.fromhex(hex_value[:-4].zfill(8)))[0]
+                    hex_value = int(hex_value.zfill(8)[:-4], base=16)
                 else:
                     pass
+
+                if address_index[addr]["label"] == "match.finished" and len(df) > 0:
+                    done = True
                 # update the current values and the data frame if a new frame passes
                 current_values_dict[address_index[addr]["label"]] = hex_value
                 if address_index[addr]["label"] == "match.frame_count":
@@ -100,7 +112,12 @@ def main():
                 """
                 # TODO: debug remove in production
                 if address_index[addr]["label"] in DEBUG_LABELS:
-                    print(addr, hex_value, address_index[addr]["label"])
+                    print(
+                        addr,
+                        hex_value,
+                        address_index[addr]["label"],
+                        address_index[addr]["type"],
+                    )
 
         except socket.timeout:
             continue
@@ -114,9 +131,6 @@ def main():
         pass
     df.to_csv("dumps/%s.csv" % dt.datetime.now())
 
-def update_df(data:np.array, df:pd.DataFrame):
-    pass
-    
 
 # address_data_pairs will take in messages from dolphin an return a list of tuples
 # that contrain (address, data) pairs.  They will be zero filled and utf-8
@@ -124,10 +138,13 @@ def update_df(data:np.array, df:pd.DataFrame):
 def address_data_pairs(msg: bytes) -> np.array:
     data = msg.strip(b"\00")
     data = data.strip(b"\n")
-    data = data.split(b"\n")
-    data = np.array(
-        list(map(lambda x: x.zfill(8).decode("utf-8"), data)), dtype="object"
-    )
+    data = data.decode("utf-8")
+    data = np.array(data.split("\n"))
+    # data = np.array(
+    #     # list(map(lambda x: x.zfill(8).decode("utf-8"), data)), dtype="object"
+    #     list(map(lambda x: x.zfill(8) if len(x) > 4 else x.zfill(4), data)),
+    #     dtype="object",
+    # )
     return data.reshape((data.size // 2, 2))
 
 
